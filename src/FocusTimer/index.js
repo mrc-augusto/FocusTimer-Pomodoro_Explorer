9ed629e9b302f7f2f4e793f3c0521c467c61193a
mrc-augusto <mrc_augusto@hotmail.com>
2023-10-03 16:59:33 -0300

    final project

--- src/FocusTimer/index.js
+++ src/FocusTimer/index.js
@@ -1,9 +1,13 @@
 import state from "./state.js";
 import * as events from './events.js';
+import * as timer from './timer.js';
 
 export function start(minutes, seconds){
   state.minutes = minutes;
   state.seconds = seconds;
 
-  events.registerControls()
-}
+  timer.updateDisplay();
+
+  events.registerControls();
+  events.setMinutes();
+};
